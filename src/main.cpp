c73816cc855e23974268d482180a90931bef310d
Pavel <korsun4@mail.ru>
2020-02-16 21:29:54 +0300

    http c++

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
-#include "socket.h"
+#include <iostream>
+#include "client.h"
 
 using namespace smtpclient;
 
-int main()
-{
-    Socket sock;
-    sock.setName("My first class socket");
-    sock.show();
+int main(int argc, char **argv)
+{   
+    if(argc > 1) {
+        Client client = Client(*(argv + 1));
+        client.send();
+    } else {
+        std::cout << "argv not initial\n";
+    }
 
     return 0;
 }
